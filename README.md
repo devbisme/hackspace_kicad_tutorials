812da20d2798539baa0f16aa7a4a8d6e1991cd4d
devbisme <devb@xess.com>
2023-11-24 12:38:00 -0500

    Initial release of HackSpace KiCad tutorial compilation.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # hackspace_kicad_tutorials
-A compilation of KiCad tutorials originally published by HackSpace magazine.
+
+This is a compilation of KiCad tutorials originally published by [HackSpace magazine](https://hackspace.raspberrypi.com/).
+
+When new tutorials are published, they can be added as follows:
+
+1. Download the issue of HackSpace that contains the tutorial and store it in the `issues` directory.
+2. Append the name of the PDF file for the issue to the `kicad_tutorial.sh` file.
+3. Run the `kicad_tutorial.sh` file to generate a new version of the `kicad_tutorial.pdf` file.
+4. Open the `kicad_tutorial.pdf` file and find the beginning page number for the newly added tutorial.
+5. Add a bookmark to the starting page of the new tutorial to the `kicad_tutorial.sh` file.
+6. Run the `kicad_tutorial.sh` file again to update the bookmarks.
---
+++ kicad_tutorial.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Create license page.
+pandoc -f markdown -t pdf << EOLICENSE > license.pdf
+These articles from [HackSpace magazine](https://hackspace.raspberrypi.com/)
+along with this compilation
+are licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)](https://creativecommons.org/licenses/by-nc-sa/3.0/us/).
+
+![CC-BY-NC-SA 3.0](https://pressbooks.bccampus.ca/posetest/wp-content/uploads/sites/1367/2021/04/by-nc-sa.png)
+EOLICENSE
+
+# Create bookmark for each tutorial in the compilation.
+cat << EOBOOKMARKS > bookmarks.txt
+BookmarkBegin
+BookmarkTitle: License
+BookmarkLevel: 1
+BookmarkPageNumber: 1
+BookmarkBegin
+BookmarkTitle: Getting started with KiCad, schematics
+BookmarkLevel: 1
+BookmarkPageNumber: 2
+BookmarkBegin
+BookmarkTitle: Getting started with KiCad, PCB layout
+BookmarkLevel: 1
+BookmarkPageNumber: 8
+BookmarkBegin
+BookmarkTitle: KiCad libraries, symbols, and footprints
+BookmarkLevel: 1
+BookmarkPageNumber: 14
+BookmarkBegin
+BookmarkTitle: KiCad: using a PCB assembly service
+BookmarkLevel: 1
+BookmarkPageNumber: 20
+BookmarkBegin
+BookmarkTitle: Designing an RP2040 board using KiCad
+BookmarkLevel: 1
+BookmarkPageNumber: 26
+BookmarkBegin
+BookmarkTitle: KiCad: schematic organisation and hierarchical sheets 
+BookmarkLevel: 1
+BookmarkPageNumber: 32
+EOBOOKMARKS
+
+# Compile license and tutorials and then add bookmarks.
+pdftk \
+	LIC=license.pdf \
+	A=issues/HS_66_Digital.pdf \
+	B=issues/HS_67_DIGITAL.pdf \
+	C=issues/HS68_DIGITAL.pdf \
+	D=issues/HS_69_DIGITAL.pdf \
+	E=issues/HS70-DIGITAL.pdf \
+	F=issues/HS71_DIGITAL.pdf \
+	cat LIC1 A54-59 B54-59 C52-57 D50-55 E52-57 F48-53 \
+	output - | \
+	pdftk - update_info bookmarks.txt output kicad_tutorial.pdf
+
+# Get rid of these as they're no longer needed.
+rm -rf license.pdf bookmarks.txt
